a89c12d51d952cedef94376a5b028a2eb8f7b714
Demidengo <dimidengo@yandex.ru>
2022-11-23 09:55:30 +0300

    Navigation Controller deleted, modal presentation added for SingleImageViewController

--- ImageFeed/SingleImage/SingleImageViewController.swift
+++ ImageFeed/SingleImage/SingleImageViewController.swift
@@ -18,19 +18,35 @@ class SingleImageViewController: UIViewController {
         return imageView
     }()
 
+    private lazy var backwardButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        let buttonImage = UIImage(named: "Backward")
+        button.setImage(buttonImage, for: .normal)
+        button.addTarget(self, action: #selector(backwardButtonAction), for: .touchUpInside)
+        return button
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .ypBlack
         layout()
     }
 
+    @objc private func backwardButtonAction() {
+        dismiss(animated: true)
+    }
+
     private func layout() {
-        view.addSubview(singleImageView)
+        [singleImageView, backwardButton].forEach { view.addSubview($0) }
         NSLayoutConstraint.activate([
-            singleImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            singleImageView.topAnchor.constraint(equalTo: view.topAnchor),
             singleImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            singleImageView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
-            singleImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+            singleImageView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            singleImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+
+            backwardButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 9),
+            backwardButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 9)
         ])
     }
 }
